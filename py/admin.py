e3397a729191bed3f9d8b524ca1709f190eaecf6
Nguyễn Hải Nam <h.nam.edu@outlook.com>
2026-06-21 16:09:20 +0700

    Update admin.py

--- py/admin.py
+++ py/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-# Importing 7 classes as requested
+# Importing all seven required classes
 from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
+# Define Inlines first
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
@@ -10,6 +11,7 @@ class QuestionInline(admin.StackedInline):
     model = Question
     extra = 3
 
+# Define Admin classes
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
     list_display = ['question_text']
@@ -17,7 +19,7 @@ class QuestionAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
-class CourseAdmin(admin.ModelAdmin):
+class CourseAdmin(admin.ModelAdmin): # Added this class
     inlines = [QuestionInline]
     list_display = ['name']
 
@@ -28,4 +30,4 @@ admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Submission)
 admin.site.register(Instructor)
-admin.site.register(Learner)
+admin.site.register(Learner) # Added registration for Learner
